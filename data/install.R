d6b34b870308abfef4df643291b968e27cd3462b
Fabian Müller <fmueller@mpi-inf.mpg.de>
2019-02-09 12:28:46 -0800

    install script link fix

---
+++ data/install.R
@@ -0,0 +1,16 @@
+## Install RnBeads
+
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager", repos="http://cran.rstudio.com")
+BiocManager::install(c("RnBeads", "RnBeads.hg19", "RnBeads.mm10"), version = "3.8", dependencies = TRUE, quiet = TRUE)
+# source("http://bioconductor.org/biocLite.R")
+# biocLite(c("RnBeads", "RnBeads.hg19", "RnBeads.mm10"), dependencies = TRUE, quiet = TRUE)
+suppressPackageStartupMessages(library(RnBeads))
+txt <- c("\nHow to test if RnBeads is correctly installed?",
+	"1. Check out our FAQ items on Ghostscript and ZIP by visiting:",
+	"https://rnbeads.org/faq.html",
+	"2. Check out the documentation:",
+	'vignette("RnBeads")',
+	"3. Start the RnBeads Data Juggler:",
+	"rnb.run.dj()\n")
+cat(paste0(txt, collapse = "\n"))
+rm(txt)
